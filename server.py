b0aeaa601c8306f671a46957a6aa4f6adc679ae8
DeltDev <deltenc88@gmail.com>
2023-11-13 10:41:00 +0700

    file and folder upload handling

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 from werkzeug.utils import secure_filename
  
 app = Flask(__name__)
- 
+
 UPLOAD_FOLDER = 'static/uploads/'
 
 app.secret_key = "secret key"
@@ -26,17 +26,30 @@ def upload_image():
         flash('No file part')
         return redirect(request.url)
     file = request.files['file']
-    database = request.files.getlist('database')
+    database = request.files.getlist("database")
+
     if file.filename == '':
         flash('Tidak ada gambar yang dipilih')
+        return redirect(request.url)
+    if not database or not any(f for f in database):
+        flash('Tidak ada file di dalam folder yang dipilih')
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        for databasefiles in database:
-            datFileName = secure_filename(databasefiles.filename)
-            databasefiles.save(os.path.join('static/imgdataset/',datFileName))
         flash('Gambar berhasil diunggah!')
+        isDatabaseValid = True
+        for databasefiles in database:
+            if(not allowed_file(databasefiles.filename)):
+                isDatabaseValid = False
+                break
+        if(isDatabaseValid):
+            for databasefiles in database:
+                datFileName = secure_filename(databasefiles.filename)
+                databasefiles.save(os.path.join('static/imgdataset/',datFileName))
+            flash("Folder berhasil diunggah!")
+        else:
+            flash('Folder yang Anda unggah mengandung file yang tidak valid. Silakan upload folder yang hanya mengandung file yang valid')
         return render_template('home.html', filename=filename)
     else:
         flash('Ekstensi file yang diperbolehkan hanyalah .jpg, .png, dan .jpeg')
